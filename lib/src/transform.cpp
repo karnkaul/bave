452ec3d7a0f89956d33d33cd4dbed356e0bee99a
Karn Kaul <karnkaul@gmail.com>
2023-12-14 18:41:18 +0530

    Fix `Transform::matrix()`.

--- lib/src/transform.cpp
+++ lib/src/transform.cpp
@@ -6,8 +6,8 @@ auto Transform::matrix() const -> glm::mat4 {
 	auto const s = glm::sin(rotation);
 	auto const c = glm::cos(rotation);
 	auto const rot = glm::mat4{
-		glm::vec4{s, c, 0.0f, 0.0f},
-		glm::vec4{c, -s, 0.0f, 0.0f},
+		glm::vec4{c, s, 0.0f, 0.0f},
+		glm::vec4{-s, c, 0.0f, 0.0f},
 		glm::vec4{0.0f, 0.0f, 1.0f, 0.0f},
 		glm::vec4{0.0f, 0.0f, 0.0f, 1.0f},
 	};
