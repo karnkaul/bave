f04bbf70b3b3aa86d117efb2eaf51443a625a352
Karn Kaul <karnkaul@gmail.com>
2023-12-29 18:49:37 +0530

    Add example/README, comments on main and flappy.

--- example/android/app/src/main/cpp/main.cpp
+++ example/android/app/src/main/cpp/main.cpp
@@ -2,9 +2,15 @@
 #include <src/flappy.hpp>
 
 extern "C" {
+// Android entry point: must have C linkage.
 void android_main(android_app* andr_app) {
+	// we will use bave::AndroidApp.
+	// create the App instance.
 	auto app = bave::AndroidApp{*andr_app};
+	// setup the entry point (Flappy).
 	app.set_game_factory([](bave::App& app) { return std::make_unique<Flappy>(app); });
+	// run App and return its exit code.
+	// after its own setup, App will create Flappy (via the factory passed above) and drive it every frame.
 	app.run();
 }
 }
--- example/desktop/main.cpp
+++ example/desktop/main.cpp
@@ -1,7 +1,10 @@
 #include <bave/desktop_app.hpp>
 #include <src/flappy.hpp>
 
+// desktop entry point
 auto main(int argc, char** argv) -> int {
+	// we will use bave::DesktopApp.
+	// set up its CreateInfo:
 	auto const create_info = bave::DesktopApp::CreateInfo{
 		.args = bave::make_args(argc, argv),
 		.title = "BaveExample",
@@ -9,7 +12,11 @@ auto main(int argc, char** argv) -> int {
 		.assets_pattern = "example/assets",
 	};
 
+	// create the App instance.
 	auto app = bave::DesktopApp{create_info};
+	// setup the entry point (Flappy).
 	app.set_game_factory([](bave::App& app) { return std::make_unique<Flappy>(app); });
+	// run App and return its exit code.
+	// after its own setup, App will create Flappy (via the factory passed above) and drive it every frame.
 	return static_cast<int>(app.run());
 }
