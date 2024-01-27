e9b26833c1a686e0e11a7e1a1930ed40602f94e4
Karn Kaul <karnkaul@gmail.com>
2024-01-27 09:59:12 +0530

    Add `ParticleEmitter`.
    - Make tests compile on limited C++20.

--- lib/include/bave/core/make_bitset.hpp
+++ lib/include/bave/core/make_bitset.hpp
@@ -4,14 +4,15 @@
 namespace bave {
 ///! Create a std::bitset with bits set.
 template <std::size_t Size, std::convertible_to<std::size_t>... I>
-constexpr auto make_bitset(I const... bits) -> std::bitset<Size> {
+auto make_bitset(I const... bits) -> std::bitset<Size> {
 	auto ret = std::bitset<Size>{};
 	(ret.set(bits), ...);
 	return ret;
 }
+
 ///! Create a std::bitset with bits set.
 template <typename Ret, std::convertible_to<std::size_t>... I>
-constexpr auto make_bitset(I const... bits) -> Ret {
+auto make_bitset(I const... bits) -> Ret {
 	return make_bitset<Ret{}.size()>(bits...);
 }
 } // namespace bave
