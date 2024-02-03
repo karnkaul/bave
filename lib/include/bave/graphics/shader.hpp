e57d3d5faaa30d220e1545b520d548fab98a15bd
Karn Kaul <karnkaul@gmail.com>
2024-02-03 19:58:55 +0530

    Add doc comments on `Shader`.

--- lib/include/bave/graphics/shader.hpp
+++ lib/include/bave/graphics/shader.hpp
@@ -11,7 +11,10 @@ class Shader {
   public:
 	static constexpr auto max_textures_v = detail::SetLayout::max_textures_v;
 
+	/// \brief The default value for line_width.
 	inline static auto default_line_width{1.0f}; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+	/// \brief The default value for polygon_mode.
+	inline static auto default_polygon_mode{vk::PolygonMode::eFill}; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 	explicit Shader(NotNull<class Renderer const*> renderer, vk::ShaderModule vertex, vk::ShaderModule fragment);
 
@@ -21,10 +24,19 @@ class Shader {
 	auto write_ubo(void const* data, vk::DeviceSize size) -> bool;
 	auto write_ssbo(void const* data, vk::DeviceSize size) -> bool;
 
+	/// \brief Draw intsances of a primitive.
+	/// \param primitive Primitive to draw.
+	/// \param instances Instances to draw.
 	void draw(RenderPrimitive const& primitive, std::span<RenderInstance::Baked const> instances);
 
+	/// \brief Line width (only relevant for vk::PolygonMode::eLine and/or Topology::eLineStrip).
+	///
+	/// Actual line width will be clamped to render device limits during draw.
 	float line_width{default_line_width};
-	vk::PolygonMode polygon_mode{vk::PolygonMode::eFill};
+	/// \brief Polygon mode.
+	///
+	/// Set to vk::PolygonMode::eLine for wireframe mode.
+	vk::PolygonMode polygon_mode{default_polygon_mode};
 
   private:
 	struct Sets {
