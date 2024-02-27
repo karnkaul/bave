b7ea1feba5bc0badb859a630a75ec0211a6e4775
Karn Kaul <karnkaul@gmail.com>
2024-02-27 11:51:45 +0530

    Invert gamepad Y axes, update GLFW gamepad mappings with custom DB.

--- lib/platform/desktop/desktop_app.cpp
+++ lib/platform/desktop/desktop_app.cpp
@@ -364,8 +364,11 @@ void DesktopApp::update_gamepads() {
 		std::memcpy(gamepad.axes.data(), axes.data(), axes.size_bytes());
 		// adjust triggers to be in [0, 1], otherwise the rest value is -1.
 		static constexpr auto project_trigger = [](float& out) { out = (out + 1.0f) * 0.5f; };
+		static constexpr auto invert_axis = [](float& out) { out = -out; };
 		project_trigger(gamepad.axes.at(GamepadAxis::eLeftTrigger));
 		project_trigger(gamepad.axes.at(GamepadAxis::eRightTrigger));
+		invert_axis(gamepad.axes.at(GamepadAxis::eLeftY));
+		invert_axis(gamepad.axes.at(GamepadAxis::eRightY));
 		gamepad.button_states = {};
 		for (std::size_t b = 0; b < buttons.size(); ++b) {
 			if (buttons[b] == 0x1) { gamepad.button_states.set(static_cast<GamepadButton>(b)); }
