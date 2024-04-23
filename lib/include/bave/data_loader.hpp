ab52c02240a8bd073c2f3b2b299869f787fd0165
Karn Kaul <karnkaul@gmail.com>
2024-04-23 12:44:54 +0530

    Update docs.

--- lib/include/bave/data_loader.hpp
+++ lib/include/bave/data_loader.hpp
@@ -6,11 +6,24 @@
 #include <vector>
 
 namespace bave {
+/// \brief Interface for loading bytes given a URI.
 class DataLoader : public Polymorphic {
   public:
+	/// \brief Check if a resource exists at the given URI.
+	/// \param uri URI of the resource.
+	/// \returns true if a resource exists.
 	[[nodiscard]] virtual auto exists(std::string_view uri) const -> bool = 0;
+
+	/// \brief Read bytes from a given URI.
+	/// \param out Storage for bytes to be read.
+	/// \param uri URI to read from.
+	/// \returns true if successful.
 	[[nodiscard]] virtual auto read_bytes(std::vector<std::byte>& out, std::string_view uri) const -> bool = 0;
 
+	/// \brief Read string from a given URI.
+	/// \param out Storage for string to be read.
+	/// \param uri URI to read from.
+	/// \returns true if successful.
 	[[nodiscard]] virtual auto read_string(std::string& out, std::string_view uri) const -> bool {
 		auto bytes = std::vector<std::byte>{};
 		if (!read_bytes(bytes, uri)) { return false; }
--- lib/include/bave/data_store.hpp
+++ lib/include/bave/data_store.hpp
@@ -1,11 +1,8 @@
 #pragma once
-#include <bave/core/c_string.hpp>
 #include <bave/data_loader.hpp>
 #include <bave/logger.hpp>
-#include <cstddef>
 #include <memory>
 #include <span>
-#include <vector>
 
 namespace dj {
 class Json;
@@ -14,16 +11,40 @@ class Json;
 namespace bave {
 class DataStore {
   public:
+	/// \brief View bytes as a string view.
+	/// \param bytes Bytes to interpret.
+	/// \returns string_view into the bytes.
 	[[nodiscard]] static auto as_string_view(std::span<std::byte const> bytes) -> std::string_view;
 
+	/// \brief Add a custom DataLoader at the given priority.
+	/// \param loader Custom DataLoader to add.
+	/// \param priority Priority to set.
+	///
+	/// The default platform DataLoader will be at priority 0.
+	/// Subsequent loaders at identical priorities will be inserted after all existing ones.
 	void add_loader(std::unique_ptr<DataLoader> loader, int priority = 0);
 
+	/// \brief Check if a resource exists at the given URI.
+	/// \param uri URI of the resource.
+	/// \returns true if a resource exists.
 	[[nodiscard]] auto exists(std::string_view uri) const -> bool;
 
+	/// \brief Read bytes from a given URI.
+	/// \param uri URI to read from.
+	/// \returns Vector of bytes, empty on failure.
 	[[nodiscard]] auto read_bytes(std::string_view uri) const -> std::vector<std::byte>;
+	/// \brief Read string from a given URI.
+	/// \param uri URI to read from.
+	/// \returns String, empty on failure.
 	[[nodiscard]] auto read_string(std::string_view uri) const -> std::string;
+	/// \brief Read JSON from a given URI.
+	/// \param uri URI to read from.
+	/// \returns JSON, null object on failure.
 	[[nodiscard]] auto read_json(std::string_view uri) const -> dj::Json;
 
+	/// \brief Convert a GLSL URI to SPIR-V.
+	/// \param glsl GLSL URI.
+	/// \returns SPIR-V URI if exists, else empty string.
 	[[nodiscard]] auto to_spir_v(std::string_view glsl) const -> std::string;
 
   private:
